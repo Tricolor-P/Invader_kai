fb110ef21ec05a9b14018613b1e60442c1724759
Tricolor-P <tricolor.base@gmail.com>
2014-06-05 18:14:28 +0900

    タイトルシーン修正
    シーンの追加の方法はこれで

--- src/main.js
+++ src/main.js
@@ -16,14 +16,16 @@ window.onload = function(){
 		rootSceneLabel.font = '14px sens-serif';
 		core.rootScene.addChild(rootSceneLabel);
 		
+		core.rootScene.backgroundColor = "#000000";
+		
 		core.rootScene.addEventListener('enterframe', function(e){
-			if(core.input.up) core.pushScene(core.titleScene);
+			if(core.input.up) core.pushScene(core.titleScene(true));
 			if(core.input.down) core.end();
 		});
 	};
 	
 	//タイトルシーンの追加
-	core.titleScene = new Title;
+	core.titleScene = function(titlePlayingIs){ return Title(titlePlayingIs); };
 
 	
 	core.start();
--- src/scene_title.js
+++ src/scene_title.js
@@ -1,18 +1,32 @@
 //タイトル画面のシーン
 
-Title = function(){
-	var scene = new Scene();
+Title = function(tp){
+	scene = new Scene();
+	scene.backgroundColor = "#000080";
 	
-	var titleLabel = new Label('タイトル');
+	//タイトルについての記述ここから	
+	titleLabel = new Label('インベーダー改（仮）');
 	titleLabel.x = core.width/2 - 14*2;
 	titleLabel.y = core.height/2 - titleLabel.height/2;
 	titleLabel.color = '#00FF00';
 	titleLabel.font = '14px sens-serif';
-	
-	scene.addChild(titleLabel);
-	scene.addEventListener('enterframe', function(e){
-		if(core.input.right) core.popScene();
+	titleLabel.startIs = tp;
+	titleLabel.endIs = false;
+	titleLabel.addEventListener('enterframe', function(e){
+		if(titleLabel.startIs){
+			titleLabel.tl.fadeIn(32).delay(16).then(function(){ titleLabel.endIs = true; }).fadeOut(32);
+			titleLabel.startIs = false;
+		}
+		
 	});
+	scene.addChild(titleLabel);
+	//タイトルについての記述ここまで
 	
+	scene.addEventListener('enterframe', function(e){
+		console.log(titleLabel.endIs);
+		if(core.input.right && titleLabel.endIs) {
+			core.popScene();
+		}
+	});
 	return scene;
 };
